0ed771ec43b7760af03bf308034408a3e8fefa64
Dobrica Pavlinusic <dpavlin@rot13.org>
2010-05-10 22:04:01 +0200

    don't emit pwd at end of sql dump

--- git-sql-insert.sh
+++ git-sql-insert.sh
@@ -1,3 +1,3 @@
 test ! -z "$GIT" && cd $GIT
 git log --pretty=format:"%H@@@%P@@@%ai@@@%s" | sed -e "s/['\\]//g" -e "s/@@@/','/g" -e "s/^/insert into log values (NULL,'/" -e "s/$/');/"
-test ! -z "$GIT" && cd -
+test ! -z "$GIT" && cd - >/dev/null
